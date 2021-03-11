ae4698f5c824d0094e2b01eb0392cc01102f9989
Neel Gaywala <neelgaywala72@gmail.com>
2021-03-11 22:27:05 +0530

    food app code changes and added token for barcode

--- food.js
+++ food.js
@@ -12,13 +12,13 @@ route.get("/", async (request, response) => {
 });
 
 //set your data
-clientID = "YOUR CLIENT ID";
-clientSecret = "YOUR CLIENT SECRET";
+clientID = "96a033ae333645abae4b24c0f2109061";
+clientSecret = "699c278daf7642438d6454ad0bfcd241";
 foodId = '33691';
 barcodeGTIN = '025293001497';
 
-// FOR ACCESS TOKEN API
-var optionsToken = {
+// FOR ACCESS TOKEN API FOOD GET
+var optionsTokenFoodGet = {
   method: "POST",
   url: "https://oauth.fatsecret.com/connect/token",
   method: "POST",
@@ -36,15 +36,46 @@ var optionsToken = {
 
 
 
-// FOR ACCESS TOKEN
+// FOR ACCESS TOKEN FODD GET
 route.get('/token', async (req, res) => {
 
-  request(optionsToken, function (error, response, body) {
+  request(optionsTokenFoodGet, function (error, response, body) {
   if (error) throw new Error(error);
   res.send(body);
   });
   
 });
+
+// FOR ACCESS TOKEN API FOOD BARCODE
+var optionsTokenFoodBarcode = {
+  method: "POST",
+  url: "https://oauth.fatsecret.com/connect/token",
+  method: "POST",
+  auth: {
+    user: clientID,
+    password: clientSecret,
+  },
+  headers: { "content-type": "application/json" },
+  form: {
+    grant_type: "client_credentials",
+    scope: "barcode",
+  },
+  json: true,
+};
+
+
+
+// FOR ACCESS TOKEN FODD BARCODE
+route.get('/tokenBarcode', async (req, res) => {
+
+  request(optionsTokenFoodBarcode, function (error, response, body) {
+  if (error) throw new Error(error);
+  res.send(body);
+  });
+  
+});
+
+
 
 
 // FOR FOOD.GET route
